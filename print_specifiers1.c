cd95bed13adee60850a0416d49224d5adce74107
Bantamlak <neoluka315@gmail.com>
2022-10-20 20:30:24 +0000

    bety style

--- print_specifiers1.c
+++ print_specifiers1.c
@@ -11,26 +11,26 @@ int print_c(va_list c)
 {
 	char ch = (char)va_arg(c, int);
 
-     _putchar(ch);
-     return (1);
+	_putchar(ch);
+	return (1);
 }
 
-  /**
-   * print_s - prints a string
-   * @s: string to print
-   *
-   * Return: number of chars printed
-   */
-int print_s(va_list s)
- {
-     int count;
-     char *str = va_arg(s, char *);
+/**
+ * print_s - prints a string
+ * @s: string to print
+ * Return: number of chars printed
+ */
 
-     if (str == NULL)
-		 str = "(null)";
-     for (count = 0; str[count]; count++)
-     {
-         _putchar(str[count]);
-     }
-     return (count);
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
