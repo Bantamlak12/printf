fee7d6501efb37ccf26dc0034b2fa5bc6a2ab0e8
Bantamlak <neoluka315@gmail.com>
2022-10-21 07:52:42 +0000

    prototypes added

--- main.h
+++ main.h
@@ -3,8 +3,8 @@
 
 #include <stdarg.h>
 /**
- * struct print - structure for printing various types
- * @t: type to print
+ * struct buffer - structure for printing various types
+ * @ch: type to print
  * @f: function to print
  */
 typedef struct buffer
@@ -13,11 +13,21 @@ typedef struct buffer
 	int (*f)(va_list);
 } buffer_t;
 
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif  /* MAIN_H */
+#endif /* MAIN_H */
